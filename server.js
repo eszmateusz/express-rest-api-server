96369da389a56560e4083578affd56279dff9c11
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-03-30 14:13:26 +0200

    Add Kodilla client's package.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const path = require('path');
 
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
