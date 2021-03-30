881006d7edc0762cfe17c5dd477f6bf8e0b8dc32
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-03-30 17:41:30 +0200

    Something wrong with yarn build version.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const path = require('path');
 
 const testimonials = require('./routes/testimonials.routes');
@@ -11,15 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Err 404 - Not found...'});
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
